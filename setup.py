08ba6f5e74bde7865015e8fa7ab962ac7e39b5ee
Wade Simmons <wsimmons@gmail.com>
2010-02-21 18:27:41 -0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(name="fitbit",
+      version="0.1",
+      description="Library for dumping data from www.fitbit.com",
+      author="Wade Simmons",
+      author_email="wade@wades.im",
+      url="http://github.com/wadey/python-fitbit",
+      packages = find_packages(),
+      license = "MIT License",
+      keywords="fitbit",
+      zip_safe = True)
